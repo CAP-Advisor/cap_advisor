d15f2306a76b37a4e4841eb505ef2bfb99955a6c
Mohammed-AbuZahra <116626380+Mohammed-AbuZahra@users.noreply.github.com>
2024-05-05 18:16:36 +0300

    user-registration

--- lib/view/sign_up_view.dart
+++ lib/view/sign_up_view.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
-class SignUpView extends StatelessWidget {
-  const SignUpView({Key? key}) : super(key: key);
+import '../view-model/sign_up_viewmodel.dart';
+import 'dart:core';
+import '../utils/validation_utils.dart';
+
+class SignUpView extends StatefulWidget {
+  @override
+  _SignUpViewState createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  final SignUpViewModel viewModel = SignUpViewModel();
+  bool isValidForm = false;
+  bool isSubmitted = false;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  bool isValidName = false;
+  bool isValidUsername = false;
+  bool isValidEmail = false;
+  bool isValidPassword = false;
+  bool isValidConfirmPassword = false;
+  bool isValidUserType = false;
+  bool emailExists = false;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -36,147 +72,142 @@ class SignUpView extends StatelessWidget {
                   ),
                   SizedBox(height: 40),
                   SizedBox(
-                    width: 370,
-                    child: DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        hintText: "User Type",
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                        fillColor: Color(0xFFF5F8F9),
-                        filled: true,
-                        hintStyle: TextStyle(
-                          color: Color(0xFF9A9A9A),
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 18,
-                        ),
-                      ),
-                      items: ["HR", "Student", "Supervisor", "Instructor"].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(
-                            value,
-                            style: TextStyle(
+                    width: 390,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        DropdownButtonFormField<String>(
+                          decoration: InputDecoration(
+                            hintText: "User Type",
+                            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide.none,
+                            ),
+                            fillColor: Color(0xFFF5F8F9),
+                            filled: true,
+                            hintStyle: TextStyle(
+                              color: Color(0xFF9A9A9A),
                               fontFamily: 'Roboto',
-                              fontWeight: FontWeight.normal,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18,
                             ),
                           ),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        // Handle dropdown value change
-                      },
+                          items: ["HR", "Student", "Supervisor", "Instructor"].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            viewModel.setUserType(newValue);
+                            setState(() {
+                              isValidUserType = newValue != null;
+                              isValidForm = validateForm();
+                            });
+                          },
+                        ),
+                        SizedBox(height: isSubmitted && !isValidUserType ? 8 : 0),
+                        Visibility(
+                          visible: isSubmitted && !isValidUserType,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 8),
+                            child: Text(
+                              "Please select a user type",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 35),
+                        buildTextField("Name", nameController, (value) {
+                          viewModel.setName(value);
+                          setState(() {
+                            isValidName = value.isNotEmpty;
+                            isValidForm = validateForm();
+                          });
+                        }, "Please enter your name", isValidName),
+                        SizedBox(height: 35),
+                        buildTextField("Username", usernameController, (value) {
+                          viewModel.setUsername(value);
+                          setState(() {
+                            isValidUsername = value.isNotEmpty;
+                            isValidForm = validateForm();
+                          });
+                        }, "Please enter your username", isValidUsername),
+                        SizedBox(height: 35),
+                        buildTextField("Email", emailController, (value) {
+                          viewModel.setEmail(value);
+                          setState(() {
+                            isValidEmail = value.isNotEmpty && ValidationUtils.isValidEmail(value);
+                            isValidForm = validateForm();
+                          });
+                        }, "Please enter a valid email", isValidEmail, keyboardType: TextInputType.emailAddress),
+                        Visibility(
+                          visible: emailExists && isSubmitted,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 8),
+                            child: Text(
+                              "Error: Email already exists",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 35),
+                        buildTextField("Password", passwordController, (value) {
+                          viewModel.setPassword(value);
+                          setState(() {
+                            isValidPassword = value.isNotEmpty && ValidationUtils.isValidPassword(value);
+                            isValidForm = validateForm();
+                          });
+                        }, "Please enter a valid password", isValidPassword, obscureText: true),
+                        SizedBox(height: 35),
+                        buildTextField("Re-enter password", confirmPasswordController, (value) {
+                          viewModel.setConfirmPassword(value);
+                          setState(() {
+                            isValidConfirmPassword = value.isNotEmpty;
+                            isValidForm = validateForm();
+                          });
+                        }, "Please re-enter your password", isValidConfirmPassword, obscureText: true),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 35),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Name",
-                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                      fillColor: Color(0xFFF5F8F9),
-                      filled: true,
-                      hintStyle: TextStyle(
-                        color: Color(0xFF9A9A9A),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 35),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Username",
-                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                      fillColor: Color(0xFFF5F8F9),
-                      filled: true,
-                      hintStyle: TextStyle(
-                        color: Color(0xFF9A9A9A),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 35),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                      fillColor: Color(0xFFF5F8F9),
-                      filled: true,
-                      hintStyle: TextStyle(
-                        color: Color(0xFF9A9A9A),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 35),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                      fillColor: Color(0xFFF5F8F9),
-                      filled: true,
-                      hintStyle: TextStyle(
-                        color: Color(0xFF9A9A9A),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 35),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Re-enter password",
-                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                      fillColor: Color(0xFFF5F8F9),
-                      filled: true,
-                      hintStyle: TextStyle(
-                        color: Color(0xFF9A9A9A),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 42),
+                  SizedBox(height: 87),
                   SizedBox(
                     width: 138,
                     height: 55,
                     child: ElevatedButton(
-                      onPressed: () {
-                        // Handle submit button press
+                      onPressed: () async {
+                        setState(() {
+                          isSubmitted = true;
+                          emailExists = false;
+                        });
+                        if (isValidForm) {
+                          bool accountCreated = await viewModel.submitForm(context);
+                          setState(() {
+                            if (viewModel.emailExists) {
+                              emailExists = true;
+                            }
+                            if (accountCreated) {
+                              // Account creation successful
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Account created successfully!'),
+                              ));
+                            } else {
+                              // Account creation failed
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Failed to create account. Please try again.'),
+                              ));
+                            }
+                          });
+                        }
                       },
+
                       child: Text(
                         "Submit",
                         style: TextStyle(
@@ -194,7 +225,6 @@ class SignUpView extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 87),
                 ],
               ),
             ),
@@ -202,5 +232,72 @@ class SignUpView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget buildTextField(String hintText, TextEditingController controller, Function(String) onChanged, String errorMessage, bool isValid, {TextInputType? keyboardType, bool obscureText = false}) {
+    String? password;
+    String? confirmPassword;
+    if (controller == passwordController) {
+      password = controller.text;
+    } else if (controller == confirmPasswordController) {
+      confirmPassword = controller.text;
+    }
+
+    String? passwordErrorMessage;
+    if (isSubmitted) {
+      if (controller == confirmPasswordController && confirmPassword != password) {
+        passwordErrorMessage = "Passwords don't match";
+      } else if ((controller == passwordController || controller == confirmPasswordController) && password != null && password.isEmpty) {
+        passwordErrorMessage = "Please enter a password";
+      } else if ((controller == passwordController || controller == confirmPasswordController) && password != null && password.length < 8) {
+        passwordErrorMessage = "Password should contain at least 8 characters";
+      }
+    }
+
+    if (passwordController.text == confirmPasswordController.text && passwordErrorMessage == "Passwords don't match") {
+      passwordErrorMessage = null;
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          controller: controller,
+          onChanged: onChanged,
+          keyboardType: keyboardType,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            hintText: hintText,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Color(0xFFF5F8F9),
+            filled: true,
+            hintStyle: TextStyle(
+              color: Color(0xFF9A9A9A),
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        SizedBox(height: (isSubmitted && isValid) || passwordErrorMessage == null ? 0 : 8),
+        Visibility(
+          visible: isSubmitted && !isValid || passwordErrorMessage != null,
+          child: Padding(
+            padding: EdgeInsets.only(top: (isSubmitted && isValid) ? 0 : 8),
+            child: Text(
+              passwordErrorMessage ?? errorMessage,
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  bool validateForm() {
+    return isValidUserType && isValidName && isValidUsername && isValidEmail && isValidPassword && isValidConfirmPassword;
   }
 }
