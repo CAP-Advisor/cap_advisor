4b5f769e4b350c0ec22bbcebbfd38aeee447e9ac
Mohammed-AbuZahra <116626380+Mohammed-AbuZahra@users.noreply.github.com>
2024-05-07 22:37:31 +0300

    adding custom widgets and using firebase auth

--- lib/service/firebase_service.dart
+++ lib/service/firebase_service.dart
@@ -1,13 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crypto/crypto.dart';
 import 'dart:convert';
+
 class FirebaseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   Future<bool> checkEmailExists(String email) async {
     try {
       QuerySnapshot querySnapshot = await _firestore
-          .collection('users')
+          .collection('Users')
           .where('email', isEqualTo: email)
           .get();
 
@@ -18,11 +18,12 @@ class FirebaseService {
     }
   }
 
-  Future<void> storeUserData(String userType, String name, String username, String email, String password) async {
+  Future<void> storeUserData(String userType, String name, String username,
+      String email, String password, String uid) async {
     try {
       String hashedPassword = _hashPassword(password);
 
-      await _firestore.collection(userType).add({
+      await _firestore.collection(userType).doc(uid).set({
         'userType': userType,
         'name': name,
         'username': username,
@@ -30,9 +31,10 @@ class FirebaseService {
         'password': hashedPassword,
       });
 
-      await _firestore.collection('users').add({
+      await _firestore.collection('Users').doc(uid).set({
         'email': email,
         'password': hashedPassword,
+        'userType': userType,
       });
 
       print('User data stored successfully in Firestore');
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final Function(String) onChanged;
+  final String errorMessage;
+  final bool isValid;
+  final bool showError;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  const CustomTextField({
+    required this.hintText,
+    required this.controller,
+    required this.onChanged,
+    required this.errorMessage,
+    required this.isValid,
+    this.showError = false,
+    this.keyboardType,
+    this.obscureText = false,
+  });
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          controller: widget.controller,
+          onChanged: widget.onChanged,
+          keyboardType: widget.keyboardType,
+          obscureText: widget.obscureText,
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Color(0xFFF5F8F9),
+            filled: true,
+            hintStyle: TextStyle(
+              color: Color(0xFF9A9A9A),
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        SizedBox(height: (widget.isValid || !widget.showError) ? 0 : 8),
+        Visibility(
+          visible: !widget.isValid && widget.showError,
+          child: Padding(
+            padding: EdgeInsets.only(top: widget.isValid ? 0 : 8),
+            child: Text(
+              widget.errorMessage,
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
