36e4ac01dd42a66ae61b710520834db6aeaa6943
HamzehSayeh1 <116834404+HamzehSayeh1@users.noreply.github.com>
2024-05-08 18:47:08 +0300

    adding logout for the users pages

--- lib/view/instructor_view.dart
+++ lib/view/instructor_view.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'login_view.dart';
 
 class InstructorView extends StatelessWidget {
+  Widget logoutBtn(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginView()));
+        },
+        child: const Text(
+          'Logout', // Changed the text to indicate the action clearly
+          style: TextStyle(color: Color(0xFF427D9D)),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Instructor View'),
+        title: const Text('Instructor View'),
       ),
       body: Center(
-        child: Text(
-          'Instructor View',
-          style: TextStyle(fontSize: 24),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 50, // You can adjust the position as needed
+              child: const Text(
+                'Instructor View',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Positioned(
+              bottom: 50, // You can adjust the position as needed
+              child: logoutBtn(context), // Correctly reference the method and pass context
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
